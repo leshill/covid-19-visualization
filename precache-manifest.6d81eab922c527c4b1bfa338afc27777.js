7323c803bfd47969e35bbbd421bafb071cd169a8
Les Hill <leshill@gmail.com>
2020-04-06 00:57:30 -0700

    Updates

---
+++ precache-manifest.6d81eab922c527c4b1bfa338afc27777.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2037f4d1ae85fb2b55116db8060fccdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14ed8c11123d0ea11aec",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "806f04368e876c4d39ed",
+    "url": "/static/css/main.6b97911a.chunk.css"
+  },
+  {
+    "revision": "14ed8c11123d0ea11aec",
+    "url": "/static/js/2.0807d50a.chunk.js"
+  },
+  {
+    "revision": "3969d1a717efe026c79cfa6f72fba35c",
+    "url": "/static/js/2.0807d50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806f04368e876c4d39ed",
+    "url": "/static/js/main.dc658eab.chunk.js"
+  },
+  {
+    "revision": "9ee759e313e321928ac4",
+    "url": "/static/js/runtime-main.3a33abc3.js"
+  }
+]);
